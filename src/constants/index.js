9867d5d74871ca42cf2705d55fe5c26f97a03b06
kaolaqi <lmq1540750371@163.com>
2019-12-22 01:10:12 +0800

    websocket服务地址修改

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,8 +8,8 @@ if (process.env.NODE_ENV === 'production') {
   host = 'http://qnz.jphd.com/ds'
   // socketUrl = 'ws://192.168.1.9:8050'
   // socketUrl = 'ws://192.168.1.6:8050'
-  socketUrl = 'ws://47.98.128.245:8050'
-  // socketUrl = 'ws://localhost:8050'
+  // socketUrl = 'ws://47.98.128.245:8050'
+  socketUrl = 'ws://localhost:8040'
 } else {
   // 测试环境地址
   host = 'http://test.jphd.com/ds'
